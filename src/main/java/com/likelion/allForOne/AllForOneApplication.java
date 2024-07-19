3f692a00e6ed3e6ad1618b81e041ac6212ddbb94
namgyuri <skarbfl0802@gmail.com>
2024-07-19 18:10:01 +0900

    init

---
+++ src/main/java/com/likelion/allForOne/AllForOneApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.allForOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllForOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllForOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/allForOne/AllForOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.allForOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllForOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
