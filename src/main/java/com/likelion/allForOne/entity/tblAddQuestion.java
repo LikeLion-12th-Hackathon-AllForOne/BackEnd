2cb7a7c2877dac1df52df8f210869e72819ff7bb
jg <ella.code.j@gmail.com>
2024-07-21 20:31:00 +0900

    Add: tblComQuestion, tblAddQuestion, tblUsedQuestion entity 생성

---
+++ src/main/java/com/likelion/allForOne/entity/tblAddQuestion.java
@@ -0,0 +1,33 @@
+package com.likelion.allForOne.entity;
+
+import com.likelion.allForOne.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class tblAddQuestion extends BaseEntity {
+    @Id
+    @Comment(value="구분자")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codeSeq;                   //구분자
+    @Column(columnDefinition = "varchar(50) not null comment '질문 내용'")
+    private String addQuestion;             //질문 내용
+
+    @ManyToOne
+    @Comment(value="질문 코드 구분자")
+    @JoinColumn(name = "code_question", nullable = false)
+    private tblCode codeQuestion;           //질문 코드 구분자
+
+    @Comment(value="질문 생성자 구분자")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_create", nullable = false)
+    private tblGroupMember memberCreate;    //질문 생성자 구분자
+}
