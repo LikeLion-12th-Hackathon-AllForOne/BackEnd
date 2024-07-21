20e9091969fabac24c3e5611856d32ffdacd9dc1
jg <ella.code.j@gmail.com>
2024-07-21 16:04:39 +0900

    Add: tblGroup entity 생성

---
+++ src/main/java/com/likelion/allForOne/entity/tblGroup.java
@@ -0,0 +1,39 @@
+package com.likelion.allForOne.entity;
+
+import com.likelion.allForOne.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class tblGroup extends BaseEntity {
+    @Id
+    @Comment(value="구분자")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupSeq;          //구분자
+    @Column(columnDefinition = "tinyint not null comment '인원수'")
+    private Integer groupMemberCnt; //인원수
+    @Column(columnDefinition = "varchar(10) not null comment '그룹명'")
+    private String groupName;       //그룹명
+    @Column(columnDefinition = "varchar(8) not null comment '그룹 초대코드'")
+    private String groupInviteCode; //그룹 초대코드
+
+
+    @Comment(value="카테고리 구분자")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "code_Category", nullable = false)
+    private tblCode codeCategory;   //카테고리 구분자
+
+    @Comment(value="방장 사용자 구분자")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_owner", nullable = false)
+    private tblUser userOwner;      //방장 사용자 구분자
+
+}
