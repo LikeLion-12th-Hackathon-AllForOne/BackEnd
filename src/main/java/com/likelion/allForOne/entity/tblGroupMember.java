7b7e7ee9917a58932e3448f941e8e2a3aa3f5f63
jg <ella.code.j@gmail.com>
2024-07-21 16:14:43 +0900

    Add: tblGroupMember entity 생성

---
+++ src/main/java/com/likelion/allForOne/entity/tblGroupMember.java
@@ -0,0 +1,36 @@
+package com.likelion.allForOne.entity;
+
+import com.likelion.allForOne.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class tblGroupMember extends BaseEntity {
+    @Id
+    @Comment(value="구분자")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberSeq;     //구분자
+
+    @Comment(value="그룹 구분자")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_seq", nullable = false)
+    private tblGroup groupSeq;  //그룹 구분자
+
+    @Comment(value="사용자 구분자")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_seq", nullable = false)
+    private tblUser userSeq;  //사용자 구분자
+
+    @ManyToOne
+    @Comment(value="편지보따리 확인코드 구분자")
+    @JoinColumn(name = "code_package", nullable = false)
+    private tblCode codePackage;  //편지보따리 확인코드 구분자
+}
