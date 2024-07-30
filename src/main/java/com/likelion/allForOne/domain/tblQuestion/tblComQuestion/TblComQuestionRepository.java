1145e93b875d3121b32a4aa2c06dbfdb95fd36b2
jg <ella.code.j@gmail.com>
2024-07-30 12:23:04 +0900

    Add: 오늘의 질문 생성하기-1-배치

---
+++ src/main/java/com/likelion/allForOne/domain/tblQuestion/tblComQuestion/TblComQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.likelion.allForOne.domain.tblQuestion.tblComQuestion;
+
+import com.likelion.allForOne.entity.TblComQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TblComQuestionRepository extends JpaRepository<TblComQuestion, Long> {
+}
---
+++ src/main/java/com/likelion/allForOne/domain/tblQuestion/tblUsedQuestion/TblUsedQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.likelion.allForOne.domain.tblQuestion.tblUsedQuestion;
+
+import com.likelion.allForOne.entity.TblUsedQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TblUsedQuestionRepository extends JpaRepository<TblUsedQuestion, Long> {
+}
